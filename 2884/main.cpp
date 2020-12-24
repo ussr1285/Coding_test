ab6df2719e1b903b0e50ac6e97f7cfc681a7c131
Minjae Cho <ussr1285@gmail.com>
2020-12-24 20:00:05 +0900

    2884 문제 품
    어릴때의 나는 왜 이걸 틀렸을까...

--- 2884/main.cpp
+++ 2884/main.cpp
@@ -1,7 +1,29 @@
 #include<iostream>
 using namespace std;
 
+#define HMAX = 24;
+#define MMAX = 60;
+
 int main(){
+    int H = 0;
+    int M = 0;
+
+    cin >> H;
+    cin >> M;
+
+    M -= 45;
+
+    if( M < 0 ){
+        H -= 1;
+        M += 60;
+    }
+
+    if( H < 0 ){
+        H += 24;
+    }
+
+    cout << H << " " << M;
+    
 
     return 0;
 }
