16b719f3d543e3964ec0d339b8902fa03c81f50b
Minjae Cho <ussr1285@gmail.com>
2021-02-17 00:31:13 +0900

    5622

--- workSpace/run.py
+++ workSpace/run.py
@@ -1,63 +1,32 @@
-def minimum_escape_maze():  # Use N, M, maze
-    cnt = 0
-
-    for _ in range(N):
-        visited.append([False for _ in range(M)])
-
-    ident_seq = deque([[0, 0]])  # bfs
-
-    while ident_seq:
-        i_j = ident_seq.pop()
-        i, j = i_j
-        possible_way = [False, False, False, False]
-
-        cnt += 1
-
-        if is_possible(i - 1, j):  # down
-            possible_way[0] = True
-        if is_possible(i, j + 1):  # right
-            possible_way[1] = True
-        if is_possible(i, j - 1):  # left
-            possible_way[2] = True
-        if is_possible(i + 1, j):  # up
-            possible_way[3] = True
-
-        # go
-        move_direction = [[i - 1, j], [i, j + 1], [i, j - 1], [i + 1, j]]
-        for _i in range(len(move_direction)):
-            if possible_way[_i]:
-                maze[move_direction[_i][0]][move_direction[_i][1]] = maze[i][j] + 1
-                ident_seq.append(move_direction[_i])
-
-        visited[i][j] = True
-
-        print("test----")
-        for t in maze:
-            print(t)
-
-    return maze[N - 1][M - 1]
+def solution(word):
+    sec = 0
+    for alphabet in word:
+        sec += translate(alphabet)
+    return sec
 
 
-def is_possible(i, j):
-    bool_result = (
-        i >= 0
-        and i < N
-        and j >= 0
-        and j < M
-        and (not visited[i][j])
-        and maze[i][j] == 1
-    )
-    return bool_result
+def translate(alphabet):
+    if alphabet == "1":
+        return 2
+    elif alphabet == "A" or alphabet == "B" or alphabet == "C":
+        return 3
+    elif alphabet == "D" or alphabet == "E" or alphabet == "F":
+        return 4
+    elif alphabet == "G" or alphabet == "H" or alphabet == "I":
+        return 5
+    elif alphabet == "J" or alphabet == "K" or alphabet == "L":
+        return 6
+    elif alphabet == "M" or alphabet == "N" or alphabet == "O":
+        return 7
+    elif alphabet == "P" or alphabet == "Q" or alphabet == "R" or alphabet == "S":
+        return 8
+    elif alphabet == "T" or alphabet == "U" or alphabet == "V":
+        return 9
+    elif alphabet == "W" or alphabet == "X" or alphabet == "Y" or alphabet == "Z":
+        return 10
+    # OPERATOR
 
 
 if __name__ == "__main__":
-    from collections import deque
-
-    N, M = list(map(int, input().split(" ")))
-    maze = []
-    visited = []
-
-    for _ in range(N):
-        maze.append(list(map(int, input())))
-
-    print(minimum_escape_maze())
+    phone_word = input()
+    print(solution(phone_word))
