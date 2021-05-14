1bf733e8279501225dbf3cab5f83fd80ca61abe9
Minjae Cho <ussr1285@gmail.com>
2021-05-14 19:23:27 +0900

    only odd

--- workSpace/main.c
+++ workSpace/main.c
@@ -25,6 +25,7 @@ void manachers(char *S, int *A){
    }
 }
 
+/*
 void addChar(char *buffer, char *S, int *len){
    S = malloc(*len * 2);
 
@@ -36,21 +37,20 @@ void addChar(char *buffer, char *S, int *len){
       S[2 * i + 1] = '#';
    }
 }
+*/
 
 int main(){
    char buffer[10001];
-   int *len;
+   int len;
    char *S;
    int *A;
 
    scanf("%s", buffer);
 
-   *len = strlen(buffer);
+   len = strlen(buffer);
 
-   /*
-      S = malloc(*len + 1);
-      strncpy(S, buffer, *len);
-   */
+   S = malloc(len + 1);
+   strncpy(S, buffer, len);
 
 // 이건 특수문자 추가한 것
    //addChar(buffer, S, len);
@@ -59,15 +59,15 @@ int main(){
 
    manachers(S, A);
 
-   int ans = -1;
-   for (int i = 0; i < *len; i++)
-      ans = max(ans, A[i]);
+   // int ans = -1;
+   // for (int i = 0; i < len; i++)
+   //   ans = max(ans, A[i]);
 
-   for(int i = 0; i < *len; i++){
+   for(int i = 0; i < len; i++){
       printf("%d ", A[i]);
    }
 
-   printf("\n%d ", ans);
+   // printf("\n%d ", ans);
 
    return 0;
 }
